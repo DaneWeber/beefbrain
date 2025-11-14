09d66f8d5a3ba20b088785296054f530952e5d9a
Dane Weber <dane.weber@excella.com>
2025-11-14 05:47:44 +0000

    Determine whether failure is formatting only

--- packages/bnb-core/src/integration.test.ts
+++ packages/bnb-core/src/integration.test.ts
@@ -1,29 +1,50 @@
 import { describe, it, expect } from '@jest/globals'
 import { readFileSync } from 'fs'
+import { parse as parseYAML } from 'yaml'
 import { updateCalculatedFields } from './index'
 
 describe('Beef Brain Core Integration', () => {
-  it('should produce unchanged output for unchanged input', () => {
-    const input = readFileSync(
-      __dirname + '/examples/unchanged/dnd35-fighter-1.yaml',
-      'utf8',
-    )
-    const expected = readFileSync(
-      __dirname + '/examples/final/dnd35-fighter-1.yaml',
-      'utf8',
-    )
-    expect(updateCalculatedFields(input)).toEqual(expected)
+  describe('keep file unchanged when updated', () => {
+    describe('dnd35-fighter-1.yaml', () => {
+      const input = readFileSync(
+        __dirname + '/examples/unchanged/dnd35-fighter-1.yaml',
+        'utf8',
+      )
+      const expected = readFileSync(
+        __dirname + '/examples/final/dnd35-fighter-1.yaml',
+        'utf8',
+      )
+
+      it('should produce equivalent output for already-correct input', () => {
+        expect(parseYAML(updateCalculatedFields(input))).toEqual(
+          parseYAML(updateCalculatedFields(expected)),
+        )
+      })
+      it('should produce identical output for already-correct input', () => {
+        expect(updateCalculatedFields(input)).toEqual(expected)
+      })
+    })
   })
 
-  it('should produce updated output for update input', () => {
-    const input = readFileSync(
-      __dirname + '/examples/update/dnd35-fighter-1.yaml',
-      'utf8',
-    )
-    const expected = readFileSync(
-      __dirname + '/examples/final/dnd35-fighter-1.yaml',
-      'utf8',
-    )
-    expect(updateCalculatedFields(input)).toEqual(expected)
+  describe('should produce updated output for update input', () => {
+    describe('dnd35-fighter-1.yaml', () => {
+      const input = readFileSync(
+        __dirname + '/examples/update/dnd35-fighter-1.yaml',
+        'utf8',
+      )
+      const expected = readFileSync(
+        __dirname + '/examples/final/dnd35-fighter-1.yaml',
+        'utf8',
+      )
+
+      it('should produce accurately updated output', () => {
+        expect(parseYAML(updateCalculatedFields(input))).toEqual(
+          parseYAML(expected),
+        )
+      })
+      it('should produce otherwise identical updated output', () => {
+        expect(updateCalculatedFields(input)).toEqual(expected)
+      })
+    })
   })
 })
